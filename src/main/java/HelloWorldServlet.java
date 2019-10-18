35e9d44186d57d27fde8c7c355f4bff4745cb3de
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-18 11:08:12 -0500

    set up and added WebServlet annotation

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,7 +1,9 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello-world")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
