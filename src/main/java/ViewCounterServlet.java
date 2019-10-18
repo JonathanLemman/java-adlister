9c90885c0b0deb1e8c4cc23e406ec7ad4819872b
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-18 12:30:29 -0500

    finished the count problem on exercise

---
+++ src/main/java/ViewCounterServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/count")
+public class ViewCounterServlet extends HttpServlet {
+
+    private int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>You have viewed this page " + count++ + " times!</h1>");
+
+    }
+}
